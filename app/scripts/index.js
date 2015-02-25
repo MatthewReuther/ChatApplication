5008644e3a555d33f487442e357b2c74879a1577
Matthew Reuther <matthew.reuther@gmail.com>
2015-02-25 10:06:37 -0600

    created a working chat app need to go back and only allow 20 chats to appear on screen

---
+++ app/scripts/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var fb = new Firebase('https://my-chatting-app.firebaseio.com/');
+
+$('form').submit(function (e) {
+  var $form = $(e.target),
+      $nameInput = $form.find('input[name="name"]'),
+      $textInput = $form.find('input[name="text"]'),
+      name   = $nameInput.val(),
+      text   = $textInput.val();
+
+  $textInput.val('');
+
+  fb.push({name: name, text: text});
+  e.preventDefault();
+});
+
+fb.on('child_added', function (snap) {
+  // fires onload once for every object in the collection,
+  // and again for additional children being added
+  var message = snap.val();
+
+  addChatMessage(message.name, message.text);
+});
+
+function addChatMessage(name, text) {
+  $('<div></div>')
+    .text(text)
+    .prepend(
+      $('<strong></strong>').text(name + ': ')
+    )
+    .appendTo($('.output'));
+}
